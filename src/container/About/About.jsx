2705a754d925c94b7b53f7ee648b1db245279409
blevy115 <32915571+blevy115@users.noreply.github.com>
2023-06-27 20:55:18 -0400

    Added appwraper to components

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,34 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
+// import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
 
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good developer.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Frontend Development",
-    description: "I am a good developer.",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Backend Development",
-    description: "I am a good developer.",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Mobile Development",
-    description: "I am a good developer.",
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
+
+// const abouts = [
+//   {
+//     title: "Web Development",
+//     description: "I am a good developer.",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Frontend Development",
+//     description: "I am a good developer.",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "Backend Development",
+//     description: "I am a good developer.",
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: "Mobile Development",
+//     description: "I am a good developer.",
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
       <h2 className="head-text">
@@ -45,7 +54,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -59,4 +68,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
